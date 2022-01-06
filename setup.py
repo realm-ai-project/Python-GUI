24e1793ffd24746702fb0cca3fd92415509db512
Ken Ming Lee <leekenming99@gmail.com>
2022-01-06 13:58:26 -0500

    add both yaml files, add realm_ai dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='realm_gui',
-    version='0.0.1',
+    version='0.0.1-beta',
     description="REALM_AI's Training Manager",
     # url='#',
     author='REALM_AI',
@@ -10,11 +10,11 @@ setup(
         'dearpygui >= 1.0.2',
         'PyYAML >= 6.0',
         "mlagents",
-        # "realm_ai" # to-be-added 
+        "realm_ai" # to-be-added 
     ],
     python_requires=">=3.7", # https://github.com/hoffstadt/DearPyGui/issues/1258
     packages=find_packages(include=['realm_gui']),
-    package_data={'': ['*.yaml']},
+    package_data={'realm_gui': ['hyperparameter_configs/*.yaml', 'ml_agents_configs/*.yaml']},
     entry_points={
         "console_scripts": [
             "realm-gui=realm_gui.main:main",
